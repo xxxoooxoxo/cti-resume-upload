0c9e4c26c02f1495b9fe7886e1116cab56b17c6e
Ali Houssain Sareini <alihsareini@live.com>
2023-10-08 03:30:12 -0400

    removed npm && resolved ts type error

--- app/api/get-blobs/route.ts
+++ app/api/get-blobs/route.ts
@@ -1,8 +1,11 @@
 import { list } from "@vercel/blob";
+import ts from "typescript";
 
 export const runtime = "edge";
 
 export async function GET(request: Request) {
   const { blobs } = await list();
+
+  // @ts-ignore - TS doesn't know about the `Response.json` property
   return Response.json(blobs);
 }
