5c9940e41ecc3c7d170a1e4a8c410975c525fce8
Ali Houssain Sareini <alihsareini@live.com>
2023-10-08 04:24:36 -0400

    pdf preview support

--- components/expanding-arrow.tsx
+++ components/expanding-arrow.tsx
@@ -27,6 +27,7 @@ export default function ExpandingArrow({ className }: { className?: string }) {
         height="16"
       >
         <path
+        
           fillRule="evenodd"
           d="M8.22 2.97a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06l2.97-2.97H3.75a.75.75 0 010-1.5h7.44L8.22 4.03a.75.75 0 010-1.06z"
         ></path>
